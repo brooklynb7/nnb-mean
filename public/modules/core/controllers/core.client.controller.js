4977154cfa95b1b0ba7639a5a0580462e4e7f690
brooklynb7 <brooklynb7@gmail.com>
2015-01-08 17:17:11 +0800

    dev

--- public/modules/core/controllers/core.client.controller.js
+++ public/modules/core/controllers/core.client.controller.js
@@ -3,6 +3,12 @@
 angular.module('core').controller('CoreController', ['$scope', '$location', 'USER_ROLES',
 	function($scope, $location, USER_ROLES) {
 		$scope.userRoles = USER_ROLES;
+
+		$scope.loading = false;
+
+		$scope.showLoading = function(isShow){
+			$scope.loading = isShow;
+		};
 		
 		$scope.$on('$stateChangeSuccess', function() {
 			if($location.path().indexOf('/wechat') === 0) {
