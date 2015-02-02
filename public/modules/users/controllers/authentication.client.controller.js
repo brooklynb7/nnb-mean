f7f7ae0e634b33b04cc25e2672f72f0f83432416
brooklynb7 <brooklynb7@gmail.com>
2015-02-02 23:32:59 +0800

    Add "remember me"
    Signed-off-by: brooklynb7 <brooklynb7@gmail.com>

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,11 +1,18 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
+angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location' ,'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
 
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
+
+		$scope.remember = function(){
+			$scope.credentials = {};
+			$scope.rememberMe = localStorage.getItem('rememberMe') === 'true' ? true:false;
+			$scope.credentials.username = localStorage.getItem('user') || '';
+			$scope.credentials.password = localStorage.getItem('pwd') || '';
+		};
 
 		$scope.signup = function() {
 			$scope.showLoading(true);
@@ -24,8 +31,18 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 
 		$scope.signin = function() {
 			$scope.showLoading(true);
+
+			
 			$http.post('./auth/signin', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
+				localStorage.setItem('rememberMe', $scope.rememberMe);
+				if($scope.rememberMe){
+					localStorage.setItem('user', $scope.credentials.username);
+					localStorage.setItem('pwd', $scope.credentials.password);
+				} else {
+					localStorage.removeItem('user');
+					localStorage.removeItem('pwd');
+				}
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
